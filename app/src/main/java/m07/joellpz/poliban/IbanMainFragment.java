aeb3e05469704020d9d60233b885015b4c67302f
joellpz <joelme001@gmail.com>
2023-02-15 23:50:35 +0100

    Calendario Guapo con Events

--- app/src/main/java/m07/joellpz/poliban/IbanMainFragment.java
+++ app/src/main/java/m07/joellpz/poliban/IbanMainFragment.java
@@ -1,24 +1,31 @@
 package m07.joellpz.poliban;
 
+import static android.content.ContentValues.TAG;
+
 import android.graphics.Color;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.core.content.ContextCompat;
 import androidx.fragment.app.Fragment;
 
-import com.github.mikephil.charting.charts.Chart;
 import com.github.mikephil.charting.charts.LineChart;
 import com.github.mikephil.charting.data.Entry;
 import com.github.mikephil.charting.data.LineData;
 import com.github.mikephil.charting.data.LineDataSet;
-import com.github.mikephil.charting.interfaces.datasets.ILineDataSet;
+import com.github.sundeepk.compactcalendarview.CompactCalendarView;
+import com.github.sundeepk.compactcalendarview.domain.Event;
 
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -75,6 +82,48 @@ public class IbanMainFragment extends Fragment {
 
         chartLoses.setData(new LineData(expediture));
         chartWins.setData(new LineData(revenue));
+
+        CompactCalendarView compactCalendar = (CompactCalendarView) view.findViewById(R.id.compactcalendar_view);
+        SimpleDateFormat dateFormatForMonth = new SimpleDateFormat("MMM - yyyy");
+
+        TextView monthText = (TextView) view.findViewById(R.id.monthText);
+        monthText.setText(dateFormatForMonth.format(compactCalendar.getFirstDayOfCurrentMonth()));
+
+        compactCalendar.setUseThreeLetterAbbreviation(true);
+
+        Event ev1 = new Event(Color.GREEN, 1676329200000L, "Some extra data that I want to store.");
+        compactCalendar.addEvent(ev1);
+
+        // Added event 2 GMT: Sun, 07 Jun 2015 19:10:51 GMT
+        Event ev2 = new Event(Color.RED, 1676329200000L);
+        compactCalendar.addEvent(ev2);
+
+        // Query for events on Sun, 07 Jun 2015 GMT.
+        // Time is not relevant when querying for events, since events are returned by day.
+        // So you can pass in any arbitary DateTime and you will receive all events for that day.
+        List<Event> events = compactCalendar.getEvents(1676329200000L); // can also take a Date object
+
+        // events has size 2 with the 2 events inserted previously
+        Log.d(TAG, "Events: " + events);
+
+
+        // define a listener to receive callbacks when certain events happen.
+        compactCalendar.setListener(new CompactCalendarView.CompactCalendarViewListener() {
+            @Override
+            public void onDayClick(Date dateClicked) {
+                List<Event> events = compactCalendar.getEvents(dateClicked);
+                Log.d(TAG, "Day was clicked: " + dateClicked + " with events " + events);
+            }
+
+            @Override
+            public void onMonthScroll(Date firstDayOfNewMonth) {
+                monthText.setText(dateFormatForMonth.format(firstDayOfNewMonth));
+                Log.d(TAG, "Month was scrolled to: " + firstDayOfNewMonth);
+            }
+        });
+
+
+
     }
 
     @Override
